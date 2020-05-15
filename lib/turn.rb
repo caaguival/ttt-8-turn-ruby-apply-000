37951015fa43fab863a85a476b5789cce1e20492
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-15 00:06:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   input_to_index(input)
-  if valid_move(board, input)
+  if valid_move?(board, input)
     move(board, input)
     puts display_board(board)
   end
